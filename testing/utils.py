0567f3c946b70fe85d6edd38192ec990bc5c0c58
Minh Nguyen <64875104+MinhxNguyen7@users.noreply.github.com>
2023-12-08 23:07:46 -0800

    Find datapoints with different labels

--- testing/utils.py
+++ testing/utils.py
@@ -27,24 +27,30 @@ def rowwise_equality(a: pd.DataFrame, b: pd.DataFrame) -> bool:
 def single_dataloaders(
     legend_path: str|Path = LEGEND_PATH, 
     img_dir: str|Path = IMG_DIR, 
+    label_name = 'emotion',
     transform: Callable[[np.ndarray], np.ndarray] = cnn_preprocess
 ) -> tuple[torch.utils.data.DataLoader, torch.utils.data.DataLoader]:
     """
-    Create two dataloaders, each with one datapoint.
+    Create two dataloaders, each with one datapoint with a different label.
     """
     
     # Only the first datapoint
     train_legend = pd.read_csv(legend_path)
     train_legend.drop(index=train_legend.index[1:], inplace=True)
 
-    # Only the second datapoint
+    # Find the first data point with a different label
     val_legend = pd.read_csv(legend_path)
-    val_legend.drop(index=val_legend.index[2:], inplace=True)
-    val_legend.drop(index=val_legend.index[0], inplace=True)
-    val_legend.reset_index(drop=True, inplace=True)
+    for i, label in enumerate(val_legend[label_name]):
+        if label != train_legend[label_name].iloc[0]:
+            val_legend.drop(index=val_legend.index[i+1:], inplace=True) # Drop all datapoints after the first different label
+            val_legend.drop(index=val_legend.index[:i], inplace=True) # Drop all datapoints before the first different label
+            break
+    else:
+        # If no different label was found
+        raise ValueError('No different label was found in the legend.')
     
     assert not rowwise_equality(train_legend, val_legend)
-
+    
     # Create dataset and dataloader
     train_dataset = Dataset(train_legend, img_dir, transform)
     val_dataset = Dataset(val_legend, img_dir, transform)
