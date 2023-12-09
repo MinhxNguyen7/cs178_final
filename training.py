fc08c9ecd18bdd9a61a2f53e3bc5a3218ea02215
Minh Nguyen <64875104+MinhxNguyen7@users.noreply.github.com>
2023-12-08 22:00:15 -0800

    Fix training code to work with new model logic

--- training.py
+++ training.py
@@ -1,10 +1,11 @@
 from config import PREFERRED_DEVICE
+from models import BaseModel
 
 import torch
 import torch.utils.data # I don't know why this isn't included in torch
 import numpy as np
 
-
+import time
 
 def evaluate(
     model: torch.nn.Module,
@@ -31,11 +32,10 @@ def evaluate(
     return total_loss / len(loader)
 
 def train(
-    model: torch.nn.Module,
+    model: BaseModel,
     train_loader: torch.utils.data.DataLoader,
     val_loader: torch.utils.data.DataLoader,
     loss_fn: torch.nn.Module,
-    optimizer: torch.optim.Optimizer,
     device: torch.device = PREFERRED_DEVICE,
     epochs: int = 1,
     log_interval: int = 100
@@ -46,6 +46,11 @@ def train(
     Example: 
         losses = train(model, train_loader, val_loader, CrossEntropyLoss(), torch.optim.SGD(model.parameters(), lr=0.0003, momentum=0.9), epochs = 10)
     """
+    if not model.optimizer:
+        raise ValueError("Model has no optimizer. Did you forget to call model.apply_optimizer()?")
+    
+    print(f"Training on device {device}")
+    
     model.train()
     model.to(device)
 
@@ -56,13 +61,15 @@ def train(
 
     for epoch in range(epochs):
         print(f"Epoch {epoch + 1} of {epochs}")
+        
+        start_time = time.time()
 
         for batch, (images, labels) in enumerate(train_loader):
             # Send to device
             images = images.to(device)
             labels = labels.to(device)
 
-            optimizer.zero_grad()
+            model.optimizer.zero_grad()
 
             # Forward pass
             outputs = model(images)
@@ -74,7 +81,7 @@ def train(
             loss.backward()
 
             # Optimize
-            optimizer.step()
+            model.optimizer.step()
 
             if batch % log_interval == 0:
                 training_loss = torch.mean(loss).item()
@@ -83,5 +90,8 @@ def train(
                 print(f"Batch {batch} of {len(train_loader)}: Training Loss = {training_loss}; Validation Loss = {validation_loss}")
                 losses["train"][epoch, batch] = training_loss
                 losses["val"][epoch, batch] = validation_loss
+                
+        end_time = time.time()
+        print(f"Epoch {epoch + 1} took {end_time - start_time} seconds")
 
     return losses
